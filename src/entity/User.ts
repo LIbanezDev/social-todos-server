e8bc80a9030683cde462fe00d4d7a49ea66370c7
lucas <lucas.vergara@usm.cl>
2020-11-06 14:55:29 -0300

    Agregar github y google auth

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,4 @@
-import {Column, Entity, OneToMany, PrimaryGeneratedColumn, BaseEntity} from "typeorm";
+import {BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import {Photo} from "./Photo";
 import {Field, ID, ObjectType} from "type-graphql";
 import {Message} from "./Message";
@@ -16,8 +16,22 @@ export class User extends BaseEntity {
     name!: string
 
     @Field()
-    @Column()
     age!: number
+
+    @Column()
+    bornDate!: Date
+
+    @Field()
+    @Column({default: '', length: 500})
+    description!: string
+
+    @Field()
+    @Column({default: false})
+    google!: boolean
+
+    @Field()
+    @Column({default: false})
+    github!: boolean
 
     @Field()
     @Column({length: 150, unique: true, nullable: false})
@@ -29,8 +43,9 @@ export class User extends BaseEntity {
     @Column({length: 30, nullable: false})
     salt!: string
 
-    @Column({length: 80, nullable: true, default: "no_image.png"})
-    image!: string
+    @Field({nullable: true})
+    @Column({length: 120, nullable: true})
+    image?: string
 
     @Field(() => [Photo])
     @OneToMany(() => Photo, photo => photo.user)
--- src/entity/input/UserRegister.ts
+++ src/entity/input/UserRegister.ts
@@ -1,21 +1,45 @@
 import {Field, InputType} from "type-graphql";
-import {User} from "../User";
+import {GraphQLUpload} from "apollo-server-express";
+import {Upload} from "../../types/graphql";
+import {registerEnumType} from "type-graphql";
 
+export enum AUTH_APPS {
+    Google,
+    GitHub,
+}
+
+registerEnumType(AUTH_APPS, {
+    name: "EXTERNAL_AUTH_APPS", // this one is mandatory
+    description: "External auth apps like GitHub or Google", // this one is optional
+});
 
 @InputType({description: "Informacion necesaria para crear nuevos usuarios"})
-export class UserRegisterInput implements Partial<User> {
+export class UserRegisterInput {
     @Field({nullable: false})
     name!: string
 
     @Field({nullable: false})
-    age!: number
-
-    @Field({nullable: false})
     email!: string
 
-    @Field({nullable: false})
+    @Field({nullable: true})
     password!: string
 
-    @Field({nullable:true})
-    image!: string
+    @Field({nullable: false})
+    bornDate!: Date
+
+    @Field(() => GraphQLUpload || Boolean, {nullable: true})
+    image?: Upload
+
+    @Field({nullable: true})
+    imageURL?: string
 }
+
+@InputType({description: "Datos necesarios para ingresar mediante una aplicacion externa como GitHub o Google"})
+export class SocialRegisterInput {
+    @Field({nullable: false})
+    token!: string
+
+    @Field(() => AUTH_APPS)
+    type!: AUTH_APPS
+}
+
