1e03c19cd2f4a0fd2d0bd970b4da5737b21b0152
lucas <lucas.vergara@usm.cl>
2020-11-06 22:33:32 -0300

    add access control allow origin

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,6 @@ class App {
         }))
         this.app.use((_, res, next) => {
             res.header('Access-Control-Allow-Origin', 'https://social-todos-web.vercel.app/');
-            res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             next()
         })
